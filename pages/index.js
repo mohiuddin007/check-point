66991431bdfecf8017b2a76d8a80bfb95b3a70a6
mohiuddin007 <mohiuddinmazumder94@gmail.com>
2022-07-21 16:20:04 +0600

    bootstrap removed and tailwind add

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import { useState } from 'react'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-
   const [textField, setTextField] = useState('')
   const [isPalindrome, setIsPalindrome] = useState(false);
 
