b91921b38e719f33c8ba99c422cec7fd1872edd5
mohiuddin007 <mohiuddinmazumder94@gmail.com>
2022-08-09 22:32:45 +0600

    ts set up

---
+++ global/mock/message.mock.ts
@@ -0,0 +1,21 @@
+export const SERVER_ERROR =
+    "SOMETHING WENT WRONG! WE DIDN'T ANTICIPATE THIS TAKING SO LONG.";
+export const UNAUTHORIZED = 'AUTHORIZATION FAILED!';
+export const SUCCESS = 'SUCCESSFULLY DONE';
+export const DEL_SUCCESS = 'SUCCESSFULLY DELETED';
+export const ACCOUNT_HOLD =
+    'Your account is on hold. Please contact with system administrator.';
+export const INVALID_USER =
+    'This account doesn’t exist, try with a different account or create a new one.';
+export const INCORRECT_PASS =
+    "The password you entered is incorrect. Please try again or select 'forgot password'";
+export const UNVERIFIED =
+    'Please activate your account. Check your email for an account activation email.';
+export const BAD_REQUEST = 'Please provider valid information';
+export const VERIFICATION_REQUEST =
+    'Please verify your email to complete registration.';
+export const ALREADY_EXIST_EMAIL =
+    'It looks like this email address is already registered. Would you like to try logged in instead?';
+export const FORGOT_PASSWORD_SUCCESS =
+    'Thanks for submitting a password request. A link to reset your password has been sent to the email you provided. Remember to check your spam folder!';
+export const PASS_UPDATE_SUCCESSFULLY = 'Password successfully updated';
